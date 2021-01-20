d15b9b2f7fabe6d8b6d7e7b87772987a5e7d8d9e
Berlik <berlik.gabdulin@tarlanpayments.kz>
2021-01-20 10:14:51 +0600

    first commit

---
+++ src/AddTodo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button } from "react-native";
+
+export const AddTodo = ({ onSubmit }) => {
+	const [value, setValue] = useState("");
+
+	const pressHandler = () => {
+		if (value.trim()) {
+			onSubmit(value);
+			setValue("");
+		} else {
+			// error
+		}
+	};
+
+	return (
+		<View style={styles.block}>
+			<TextInput
+				onChangeText={setValue}
+				value={value}
+				placeholder="Введите название дела..."
+				style={styles.input}
+			/>
+			<Button title="Добавить" onPress={pressHandler} />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	block: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginBottom: 15,
+	},
+	input: {
+		width: "70%",
+		padding: 10,
+		borderStyle: "solid",
+		borderBottomWidth: 2,
+		borderBottomColor: "#3949ab",
+	},
+});
