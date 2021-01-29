5ee94ceec940e44534fcf355ea3dc59e543d378f
Berlik <berlik.gabdulin@tarlanpayments.kz>
2021-01-29 17:48:34 +0600

    Block 3. New screens

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import { AddTodo } from "./src/AddTodo";
-import { Navbar } from "./src/Navbar";
-import { Todo } from "./src/Todo";
+import { StyleSheet, View } from "react-native";
+import { Navbar } from "./src/components/Navbar";
+import { MainScreen } from "./src/screens/MainScreen";
 
 export default function App() {
 	const [todos, setTodos] = useState([]);
@@ -39,12 +38,7 @@ export default function App() {
 		<View>
 			<Navbar title="ToDo App!" />
 			<View style={styles.container}>
-				<AddTodo onSubmit={addTodo} />
-				<FlatList
-					keyExtractor={(item) => item.id.toString()}
-					data={todos}
-					renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} />}
-				/>
+				<MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo} />
 			</View>
 		</View>
 	);
